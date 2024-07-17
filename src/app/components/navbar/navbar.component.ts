6c722fd4a7346b112021f957556df7065d6fae8c
Juliana Cardona <julicardonco@hotmail.com>
2024-07-17 18:53:18 -0500

    Board accordion

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import { BtnComponent } from '../btn/btn.component';
 import { OverlayModule } from '@angular/cdk/overlay';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBell, faInfoCircle } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [BtnComponent, OverlayModule],
+  imports: [BtnComponent, OverlayModule, FontAwesomeModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
 
+    faBell = faBell;  
+    faInfoCirle = faInfoCircle;
     isOpen = false;
+    isOpenBody = false;
 }
