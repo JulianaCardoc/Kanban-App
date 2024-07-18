e0514fa8394d315d29a795a07beec88d9c9a8142
Juliana Cardona Cortes <julianacardonacortes@MacBook-Pro-de-Charly.local>
2024-07-18 07:36:21 -0500

    Board cards and dialogs

--- src/app/components/btn/btn.component.ts
+++ src/app/components/btn/btn.component.ts
@@ -15,12 +15,21 @@ export class BtnComponent {
 
   get colors() {
     return {
+      'text-white': this.color === 'success' || this.color === 'primary' || this.color === 'red',
+      'text-gray-700': this.color === 'gray-light',
       'bg-succes-700': this.color === 'success',
       'hover:bg-succes-800': this.color === 'success',
       'focus:ring-succes-300': this.color === 'success',
       'bg-primary-700': this.color === 'primary',
       'hover:bg-primary-800': this.color === 'primary',
       'focus:ring-primary-300': this.color === 'primary',
+      'bg-red-700': this.color === 'red',
+      'hover:bg-red-800': this.color === 'red',
+      'focus:ring-red-300': this.color === 'red',
+      'bg-gray-200': this.color === 'gray-light',
+      'hover:bg-gray-500': this.color === 'gray-light',
+      'focus:ring-gray-50': this.color === 'gray-light'
+
     }
   }
 }
